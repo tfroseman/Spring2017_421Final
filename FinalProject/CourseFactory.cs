a5756618f9df8ed8fbad1f69e0b03d4c8c9486bc
Thomas Roseman <roseman.tom@gmail.com>
2017-04-27 14:09:20 -0400

    Added some info to the factory on how to use Dapper

--- FinalProject/CourseFactory.cs
+++ FinalProject/CourseFactory.cs
@@ -12,6 +12,9 @@ namespace FinalProject
 {
     class CourseFactory
     {
+        /// <summary>
+        /// SQLite database connection
+        /// </summary>
         SQLiteConnection database = Database.SimpleDbConnection();
        
         /// <summary>
@@ -21,8 +24,14 @@ namespace FinalProject
         /// <returns>An inflated course object</returns>
         public Course getClass(String ID)
         {
+            //Open connection to database
             database.Open();
+
+            //Use Dapper to query database using sql. @ is used for string replacement.
             var course = database.Query<Course>("select title, description, number = @courseID, school, instructor, required FROM Course", new { courseID = ID });
+
+            // course will be an IEnumerable object so in this case we want the first object.
+            // We could also do course.First().ID to get the ID value from the Course as well
             return course.First();
         }
     }
--- FinalProject/Form1.Designer.cs
+++ FinalProject/Form1.Designer.cs
@@ -193,7 +193,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.IndianRed;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(817, 510);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
