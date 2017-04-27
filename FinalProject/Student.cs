d58d500ed8c730a97bc62314e69e65556f09f433
Thomas Roseman <roseman.tom@gmail.com>
2017-04-27 11:14:03 -0400

    Master code base push

--- FinalProject/Student.cs
+++ FinalProject/Student.cs
@@ -1,7 +1,51 @@
-﻿namespace FinalProject
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
 {
-    public class Student
+    class Student : Student_IF
     {
-        //Student changes
+        private StudentSchedule schedule;
+        private String name;
+        private Gender gender;
+        private School school;
+        private String DOB;
+        private int completedCredits;
+        private MajorDecorater major;
+        public int getCredits()
+        {
+            return completedCredits;
+        }
+
+        public string getDOB()
+        {
+            return DOB;
+        }
+
+        public MajorDecorater getMajor()
+        {
+            return major;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public StudentSchedule getSchedule()
+        {
+            return schedule;    
+        }
+
+        ///TODO logic for next state here
+        public Student getNextYear()
+        {
+            
+            return null;
+        }
+      
     }
 }
